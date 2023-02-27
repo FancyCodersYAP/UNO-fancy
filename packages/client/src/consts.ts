d160ab75f2a68d76a5c146286deabb77871f0da7
Valentin Erdyakov <valentin@Mac-mini-Valentin.local>
2023-02-27 15:42:03 +0500

    Добавляет попап завершения игры

---
+++ packages/client/src/consts.ts
@@ -0,0 +1,10 @@
+export const AppRoute = {
+  MAIN: `/`,
+  LOGIN: `/login`,
+  REGISTRATION: `/registration`,
+  PROFILE: `/profile`,
+  LEADERBOARD: `/leaderboard`,
+  GAME: `/game`,
+  FORUM: `/forum`,
+  NOT_FOUND_PAGE: `*`,
+}
