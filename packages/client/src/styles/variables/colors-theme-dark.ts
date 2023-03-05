5be8602b4015c85183498a106a90f09abb8f1255
Iuliia Stetskaia <juliastetskaya@gmail.com>
2023-03-05 12:09:42 +0300

    feat: Add leaderboard

--- packages/client/src/styles/variables/colors-theme-dark.ts
+++ packages/client/src/styles/variables/colors-theme-dark.ts
@@ -5,10 +5,13 @@ export const COLOR_ELEMENT_SECONDARY = '#9ba9b8';
 export const COLOR_TEXT_SECONDARY = 'rgba(216,214,214,0.71)';
 export const COLOR_ELEMENT_WARN = '#dc2f55';
 export const COLOR_BACKGROUND_SECONDARY = '#515151';
+export const COLOR_TABLE_BACKGROUND_PRIMARY = '#155d7e';
+export const COLOR_TABLE_BACKGROUND_SECONDARY = '#515151';
+export const COLOR_TABLE_TEXT_PRIMARY = '#ffffff';
 
 export const name = 'dark';
 
 export const DARKEN = `{
-      background-color: rgba(50,50,50,.5);
-      background-blend-mode: multiply;
-    }`;
+  background-color: rgba(50,50,50,.5);
+  background-blend-mode: multiply;
+}`;
