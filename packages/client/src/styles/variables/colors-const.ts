b77ee8f6829bf5f1f3043a19d4b392411d83b20e
Aleksei Belousov <vs.oko@rambler.ru>
2023-03-05 23:45:35 +0300

    team review fixes

--- packages/client/src/styles/variables/colors-const.ts
+++ packages/client/src/styles/variables/colors-const.ts
@@ -6,9 +6,3 @@ export const BLUE_COLOR = '#0E7A89FF';
 export const GREY_2 = '#c6cedb';
 
 export const GREY_3 = '#d7dbdb';
-
-export const GREY_4 = '#D9D9D9';
-
-export const GREY_5 = '#D9D9D9';
-
-export const GREY_6 = '#D9D9D9';
