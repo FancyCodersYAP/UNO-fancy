0b73bb50270410bcea0db71f000be21ec40583b7
Dormeh <dormeh@icloud.com>
2023-03-05 17:04:33 +0300

    add theme neutral colors-const.ts change hex colors to variables in Toggler style

---
+++ packages/client/src/styles/variables/colors-const.ts
@@ -0,0 +1,14 @@
+//тля цветов которые не будут меняться по теме
+export const WHITE_COLOR = '#fff';
+
+export const BLUE_COLOR = '#0E7A89FF';
+
+export const GREY_2 = '#c6cedb';
+
+export const GREY_3 = '#d7dbdb';
+
+export const GREY_4 = '#D9D9D9';
+
+export const GREY_5 = '#D9D9D9';
+
+export const GREY_6 = '#D9D9D9';
