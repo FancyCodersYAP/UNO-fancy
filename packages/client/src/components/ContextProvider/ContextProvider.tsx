cd3304c5b483cc6f19905fb0672b6ac43c8bb63f
Aleksei Belousov <vs.oko@rambler.ru>
2023-03-06 18:02:22 +0300

    AppContext typed fix

--- packages/client/src/components/ContextProvider/ContextProvider.tsx
+++ packages/client/src/components/ContextProvider/ContextProvider.tsx
@@ -5,18 +5,10 @@ interface IThemeContext {
   handleThemeChange?: () => void;
 }
 
-type TContext = {
-  data: IThemeContext;
-  children: React.ReactNode;
-};
 const defaultState = {
   themeTogglerState: false,
 };
 
 const AppContext = React.createContext<IThemeContext>(defaultState);
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-function contextProvider({ data, children }: TContext) {
-  return <AppContext.Provider value={data}>{children}</AppContext.Provider>;
-}
 
 export default AppContext;
