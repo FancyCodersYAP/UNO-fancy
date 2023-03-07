cb044ccd7c8561bbf1c52cf935eddfc1d5a70fe2
Valentin Erdyakov <valentin@Mac-mini-Valentin.local>
2023-03-07 22:23:12 +0500

    Embeds Link and TextContainer component

--- packages/client/src/components/startGame/startGame.tsx
+++ packages/client/src/components/startGame/startGame.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from 'react-router-dom';
 import { AppRoute } from 'utils/constants';
-import { StStartGame, StStartGameWrapper, StStartGameTitle, StStartGameImgs, StStartGameImgWrapper, StStartGameImg, StStartGameText, StStartGameButtons, StStartGameLink, StStartGameButton } from './style';
+import Button from 'components/Button/Button';
+import { StLink, StTextContainer } from 'styles/global';
+import { StStartGame, StStartGameWrapper, StStartGameTitle, StStartGameImgs, StStartGameImgWrapper, StStartGameImg, StStartGameNavWrapper } from './style';
 
 const StartGame = () => {
   const navigate = useNavigate()
@@ -15,25 +17,25 @@ const StartGame = () => {
         <StStartGameTitle>Правила игры:</StStartGameTitle>
         <StStartGameImgs>
           <StStartGameImgWrapper>
-            <StStartGameImg src="src/assets/img/start-game-1.jpg" alt="Карточка правил" width="160px" height="160px" />
-            <StStartGameText>Избавляйтесь от карт одного цвета в первую очередь</StStartGameText>
+            <StStartGameImg src="src/assets/img/start-game-1.jpg" alt="Карточка правил" />
+            <StTextContainer>Избавляйтесь от карт одного цвета в первую очередь</StTextContainer>
           </StStartGameImgWrapper>
 
           <StStartGameImgWrapper>
-            <StStartGameImg src="src/assets/img/start-game-2.jpg" alt="Карточка правил" width="160px" height="160px" />
-            <StStartGameText>Следите за ходами противника</StStartGameText>
+            <StStartGameImg src="src/assets/img/start-game-2.jpg" alt="Карточка правил" />
+            <StTextContainer>Следите за ходами противника</StTextContainer>
           </StStartGameImgWrapper>
 
           <StStartGameImgWrapper>
-            <StStartGameImg src="src/assets/img/start-game-3.jpg" alt="Карточка правил" width="160px" height="160px" />
-            <StStartGameText>Используйте карты действий в более подходящий момент</StStartGameText>
+            <StStartGameImg src="src/assets/img/start-game-3.jpg" alt="Карточка правил" />
+            <StTextContainer>Используйте карты действий в более подходящий момент</StTextContainer>
           </StStartGameImgWrapper>
         </StStartGameImgs>
 
-        <StStartGameButtons>
-          <StStartGameLink href="https://inteltoys.ru/articles/cat7/article655.html" target="_blank">Подробнее о правилах</StStartGameLink>
-          <StStartGameButton onClick={navigateToGame} primary>Начать</StStartGameButton>
-        </StStartGameButtons>
+        <StStartGameNavWrapper>
+          <StLink to="https://inteltoys.ru/articles/cat7/article655.html" target="_blank">Подробнее о правилах</StLink>
+          <Button onClick={navigateToGame} text="Начать" primary />
+        </StStartGameNavWrapper>
       </StStartGameWrapper>
     </StStartGame>
   )
--- packages/client/src/components/startGame/style.tsx
+++ packages/client/src/components/startGame/style.tsx
@@ -1,9 +1,8 @@
 import styled from 'styled-components';
 import * as COLORS from '../../styles/variables/colors-const';
-import { StButton } from 'components/Button/Button';
 
 export const StStartGame = styled.section`
-  background-color: ${COLORS.BLACK_OPACITY_05};
+  background-color: ${COLORS.BACKGROUND_ELEMENT_OPACITY};
   width: 100vw;
   height: 100vh;
   position: relative;
@@ -41,34 +40,12 @@ export const StStartGameImgWrapper = styled.div`
 `;
 
 export const StStartGameImg = styled.img`
-  max-width: 160px;
-  max-height: 160px;
   width: 160px;
   height: 160px;
   margin-bottom: 10px;
 `;
 
-export const StStartGameText = styled.span`
-  font-size: 16px;
-  text-align: center;
-  color: ${props => props?.theme.COLOR_TEXT_PRIMARY};
-`;
-
-export const StStartGameButtons = styled.div`
+export const StStartGameNavWrapper = styled.div`
   display: flex;
   justify-content: space-between;
-`;
-
-export const StStartGameLink = styled.a`
-  text-decoration: none;
-  color: ${props => props?.theme.COLOR_TEXT_PRIMARY};
-  text-shadow: 3px 3px 3px rgba(0, 0, 0, 0.25);
-
-  &:hover {
-    opacity: 0.6;
-  }
-`;
-
-export const StStartGameButton = styled(StButton)`
-  min-width: 150px;
 `;
