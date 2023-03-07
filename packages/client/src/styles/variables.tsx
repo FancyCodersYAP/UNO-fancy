2269fa7195570ec7eea5cceb0c3fceb34661d5bb
Valentin Erdyakov <valentin@Mac-mini-Valentin.local>
2023-03-07 22:35:22 +0500

    update package.json

--- packages/client/src/styles/variables.tsx
+++
@@ -1,7 +0,0 @@
-export const variables = {
-  backgroundFormColor: '#e8c40a',
-  borderRadius: '20px',
-  primaryButtonColor: '#6b6b6a',
-  buttonColor:'#23B7CB',
-  backgroundPopupColor: 'rgba(0, 0, 0, 0.5)',
-};
