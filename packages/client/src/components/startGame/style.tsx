57884a35e95a573887ed5c023b83fba4c85766ae
Valentin Erdyakov <valentin@Mac-mini-Valentin.local>
2023-03-09 19:48:15 +0500

    Add Modal component

--- packages/client/src/components/startGame/style.tsx
+++ packages/client/src/components/startGame/style.tsx
@@ -1,35 +1,4 @@
 import styled from 'styled-components';
-import * as COLORS from '../../styles/variables/colors-const';
-
-export const StStartGame = styled.section`
-  background-color: ${COLORS.BACKGROUND_ELEMENT_OPACITY};
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  position: fixed;
-  z-index: 1;
-`;
-
-export const StStartGameWrapper = styled.div`
-  background-color: ${props => props?.theme.COLOR_PREVIEW_PRIMARY};
-  border-radius: 20px;
-  position: absolute;
-  top: 50%;
-  left: 50%;
-  transform: translate(-50%, -50%);
-  padding: 40px;
-  min-width: 600px;
-`;
-
-export const StStartGameTitle = styled.h3`
-  color: ${props => props?.theme.COLOR_TEXT_PRIMARY};
-  font-size: 26px;
-  font-weight: 600;
-  text-align: center;
-  margin: 0;
-  margin-bottom: 50px;
-`;
 
 export const StStartGameImgs = styled.div`
   display: flex;
