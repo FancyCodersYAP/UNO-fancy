6425c2f248714b3ff98abd8dc1bac8387550f774
Marianna <mnapolskih@gmail.com>
2023-03-17 16:38:29 +0300

    set routing with different layouts

--- packages/client/src/components/Layout/GameLayout.tsx
+++ packages/client/src/components/Layout/GameLayout.tsx
@@ -1,14 +1,15 @@
 import React, { FC } from 'react';
 import styled from 'styled-components';
+import { Outlet } from 'react-router-dom';
 
 const StGameScreen = styled.div``;
 
 type LayoutProps = {
-  children: React.ReactNode;
+  children?: React.ReactNode;
 };
 
 const GameLayout: FC<LayoutProps> = ({ children }) => (
-  <StGameScreen>{children}</StGameScreen>
+  <StGameScreen>{children ?? <Outlet />}</StGameScreen>
 );
 
 export default GameLayout;
