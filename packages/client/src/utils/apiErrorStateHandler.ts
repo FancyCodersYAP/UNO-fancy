90b443b7cd6e0a1d418355786f82b55fe5815739
Dormeh <dormeh@icloud.com>
2023-04-08 18:08:09 +0300

    file apiErrorStateHandle rename to apiErrorStateHandler

--- packages/client/src/utils/apiErrorStateHandler.ts
+++ packages/client/src/utils/apiErrorStateHandler.ts
@@ -1,11 +1,11 @@
 import { PayloadAction } from '@reduxjs/toolkit';
 import { IErrorReason } from '../store/types';
 
-const apiErrorStateHandle = (
+const apiErrorStateHandler = (
   action: PayloadAction<string | IErrorReason>
 ): string =>
   typeof action.payload === 'object' && 'reason' in action.payload
     ? action.payload.reason
     : action.payload;
 
-export default apiErrorStateHandle;
+export default apiErrorStateHandler;
