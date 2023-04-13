7eb17c93f22866831491019f93c18130e5560e26
Valentin Erdyakov <valentin@Mac-mini-Valentin.local>
2023-04-13 16:16:53 +0500

    fix Timer

--- packages/client/src/components/Timer/Timer.tsx
+++ packages/client/src/components/Timer/Timer.tsx
@@ -3,20 +3,11 @@ import useTimer from 'hooks/useTimer';
 import { StTimer, StTimerContainer } from './style';
 
 const Timer = () => {
-  const { timer, handleStart, handlePause, handleResume, handleReset } =
-    useTimer(0);
+  const { timer } = useTimer(0);
 
   return (
     <StTimer>
       <StTimerContainer>{formatTime(timer)}</StTimerContainer>
-
-      {/* Временный код для проверки таймера */}
-      {/* <div>
-        <button onClick={handleStart}>Start</button>
-        <button onClick={handlePause}>Pause</button>
-        <button onClick={handleResume}>Resume</button>
-        <button onClick={handleReset}>Reset</button>
-      </div> */}
     </StTimer>
   );
 };
--- packages/client/src/utils/formatTime.tsx
+++ packages/client/src/utils/formatTime.tsx
@@ -1,7 +1,7 @@
 const formatTime = (timer: number) => {
   const getSeconds = `0${timer % 60}`.slice(-2);
-  const minutes = `${Math.floor(timer / 60)}`;
-  const getMinutes = `0${Number(minutes) % 60}`.slice(-2);
+  const minutes = Math.floor(timer / 60);
+  const getMinutes = `0${minutes % 60}`.slice(-2);
   const getHours = `0${Math.floor(timer / 3600)}`.slice(-2);
 
   if (timer > 3599) {
