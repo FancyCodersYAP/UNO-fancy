8a17da1f5504cfb81cfbefd8468534d472393452
Dormeh <dormeh@icloud.com>
2023-04-19 15:58:40 +0300

    fix errors with TS for build

--- packages/client/src/client.d.ts
+++ packages/client/src/client.d.ts
@@ -1,12 +1,10 @@
+import {} from 'styled-components/cssprop';
+
 declare global {
   interface Window {
     __PRELOADED_STATE__?: Record<string, Record<string, unknown>>;
     webkitAudioContext: typeof AudioContext;
   }
-
-  export type Nullable<T> = T | null;
-
-  export type TupleArray<T, len extends number> = [T, ...T[]] & { length: len };
 }
 
 export {};
