f48c167583e8ca40917c0b2fc6217617118e495f
Valentin Erdyakov <valentin@Mac-mini-Valentin.local>
2023-04-20 11:06:18 +0300

    add canvas cards

--- packages/client/src/pages/NotFoundPage/style.ts
+++ packages/client/src/pages/NotFoundPage/style.ts
@@ -2,15 +2,14 @@ import styled from 'styled-components';
 
 
 export const St404Wrapper = styled.div`
-  width: 450px;
+  //width: 450px;
   border-radius: 50px;
   background-color: ${props => props.theme?.COLOR_PREVIEW_PRIMARY};
-  padding: 40px;
+  padding: 50px 60px;
 
   p {
     color: ${props => props.theme?.COLOR_TEXT_PRIMARY};
-    margin-bottom: 40px;
+    margin-bottom: 60px;
     text-align: center;
   }
 `;
-
