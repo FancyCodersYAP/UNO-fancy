ef6844d4bc6974cdbf53fd6587eff735fef10200
Valentin Erdyakov <valentin@Mac-mini-Valentin.local>
2023-04-21 13:33:40 +0300

    rearrangement of cards

--- packages/client/src/pages/NotFoundPage/style.ts
+++ packages/client/src/pages/NotFoundPage/style.ts
@@ -1,15 +1,16 @@
 import styled from 'styled-components';
 
-
 export const St404Wrapper = styled.div`
-  //width: 450px;
   border-radius: 50px;
   background-color: ${props => props.theme?.COLOR_PREVIEW_PRIMARY};
   padding: 50px 60px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
 
   p {
     color: ${props => props.theme?.COLOR_TEXT_PRIMARY};
-    margin-bottom: 60px;
+    margin: 20px 0 60px;
     text-align: center;
   }
 `;
