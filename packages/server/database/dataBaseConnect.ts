c63b8e87e414dea456782f0aef0819e26b8445f3
Dormeh <dormeh@icloud.com>
2023-04-25 18:42:26 +0300

    fix texts of some logs else

--- packages/server/database/dataBaseConnect.ts
+++ packages/server/database/dataBaseConnect.ts
@@ -45,7 +45,7 @@ export const initPostgresDBConnection = async (): Promise<
 
     console.log('  ➜ 🎸 Connected to the Postgres database');
   } catch (e) {
-    console.log('🌐 DB Connection error ⛔️');
+    console.log('  ➜🌐 DB Connection error ⛔️');
   }
 
   return client;
