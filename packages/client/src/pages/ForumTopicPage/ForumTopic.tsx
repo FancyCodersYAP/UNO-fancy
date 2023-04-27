e851c97a55c7ed717c10a019df4dc4324a4345ae
Dormeh <dormeh@icloud.com>
2023-04-27 17:28:48 +0300

    add topicActions add api get topic data from DB

--- packages/client/src/pages/ForumTopicPage/ForumTopic.tsx
+++ packages/client/src/pages/ForumTopicPage/ForumTopic.tsx
@@ -20,6 +20,11 @@ import {
   StTopicDiscussion,
 } from './style';
 import useModal from 'hooks/useModal';
+import { useParams } from 'react-router-dom';
+import { useAppDispatch, useAppSelector } from '../../hooks/redux';
+import { useEffect } from 'react';
+import { fetchForumTopicGetById } from '../../store/Forum/topicActions';
+import { dateStringParse } from 'utils/dateStringParse';
 
 const marginBottom58px = css`
   margin: 0 0 58px;
@@ -27,32 +32,47 @@ const marginBottom58px = css`
 
 const ForumTopic = () => {
   const { isOpen, handleOpenModal, handleCloseModal } = useModal();
+  const { id } = useParams();
 
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    if (id) {
+      dispatch(fetchForumTopicGetById(id));
+    }
+  }, []);
+  const TopicContent = useAppSelector(state => state.FORUM.currentTopic);
+  const messages = [];
+
+  console.log(TopicContent);
+  if (!TopicContent) return <></>;
   return (
     <StBoard css={stBoardStyle}>
       <StTitle css={marginBottom58px}>Форум</StTitle>
 
       <StTopic>
         <StUser>
-          <StUserAvatar image={testTopicData.avatar} />
+          <StUserAvatar image={TopicContent.user.avatar} />
           <StUserInfo>
-            <StUserName>{testTopicData.author}</StUserName>
+            <StUserName>{TopicContent.user.display_name}</StUserName>
             <StUserRank>{testTopicData.rank}</StUserRank>
           </StUserInfo>
         </StUser>
         <StTopicWrapper>
           <StTopicNameContainer>
-            <StTopicName>{testTopicData.topicName}</StTopicName>
+            <StTopicName>{TopicContent.name}</StTopicName>
           </StTopicNameContainer>
           <StTopicText>
-            {testTopicData.messages}
-            <StTopicDate>тема создана: {testTopicData.date}</StTopicDate>
+            {TopicContent.description}
+            <StTopicDate>
+              тема создана: {dateStringParse(TopicContent.created_at)}
+            </StTopicDate>
           </StTopicText>
         </StTopicWrapper>
       </StTopic>
 
       <StTopicDiscussion>
-        {testTopicDiscussionData.map(message => (
+        {TopicContent.messages.map(message => (
           <TopicMessage
             key={message.id}
             {...message}
