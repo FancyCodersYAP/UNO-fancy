92fde1bee4e2ce27bb5515ea39832bff9871e0eb
Valentin Erdyakov <valentin@Mac-mini-Valentin.local>
2023-04-30 18:37:30 +0500

    remove canBeClosedOutside props

--- packages/client/src/pages/ForumPage/ForumPage.tsx
+++ packages/client/src/pages/ForumPage/ForumPage.tsx
@@ -78,11 +78,7 @@ const ForumPage = () => {
       </StTable>
 
       {isOpen && (
-        <Modal
-          title="Создание темы"
-          styles={addTopicModalStyles}
-          handleCloseModal={handleCloseModal}
-          canBeClosedOutside>
+        <Modal title="Создание темы" styles={addTopicModalStyles}>
           <AddTopic handleCloseModal={handleCloseModal} />
         </Modal>
       )}
