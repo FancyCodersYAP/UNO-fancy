d4f7b6fc6e76488ce35c263de057e85494041849
Dormeh <dormeh@icloud.com>
2023-05-08 18:42:42 +0300

    splits the theme route into a route and a controller

---
+++ packages/server/controllers/userThemes.ts
@@ -0,0 +1,23 @@
+import { UserThemes } from '../models/UserThemes';
+import { Themes } from '../models/Themes';
+
+export const themeGet = async (user_id: string) => {
+  return await UserThemes.findOne({
+    where: { user_id },
+    include: [{ model: Themes, attributes: ['theme_name'] }],
+  });
+};
+
+export const themePost = async (theme_name: string, user_id: string) => {
+  const theme = await Themes.findOne({
+    where: { theme_name },
+  });
+
+  return (
+    theme &&
+    (await UserThemes.upsert({
+      theme_id: theme.id,
+      user_id,
+    }))
+  );
+};
