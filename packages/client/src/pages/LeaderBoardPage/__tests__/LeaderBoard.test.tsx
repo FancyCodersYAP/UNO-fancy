10ff105f6774ba6fcf4adb733b83d07f23eba26f
catalinadevolg <kattti94@mail.ru>
2023-05-12 10:57:54 +0400

    Merge branch 'main' into feature/FC-53_unit_tests

--- packages/client/src/pages/LeaderBoardPage/__tests__/LeaderBoard.test.tsx
+++ packages/client/src/pages/LeaderBoardPage/__tests__/LeaderBoard.test.tsx
@@ -1,74 +1,84 @@
 import { render, screen } from '@testing-library/react';
 import LeaderBoard from '../LeaderBoard';
+import { setupStore } from '../../../store/store';
+import { UserService } from 'api/UserService';
+import { YandexAPIRepository } from '../../../repository/YandexAPIRepository';
+import { Provider } from 'react-redux';
 
-const players = [
-  {
-    id: '1',
-    name: 'Ivan',
-    score: 115,
-    time: '1:15',
-  },
-  {
-    id: '2',
-    name: 'Roman',
-    score: 318,
-    time: '2:45',
-  },
-  {
-    id: '3',
-    name: 'Ksenia',
-    score: 760,
-    time: '3:05',
-  },
-];
+const store = setupStore(new UserService(new YandexAPIRepository()));
 
-jest.mock('lodash/orderBy', () => ({
-  default: jest.fn(fn => fn),
-}));
+// const players = [
+//   {
+//     id: '1',
+//     name: 'Ivan',
+//     score: 115,
+//     time: '1:15',
+//   },
+//   {
+//     id: '2',
+//     name: 'Roman',
+//     score: 318,
+//     time: '2:45',
+//   },
+//   {
+//     id: '3',
+//     name: 'Ksenia',
+//     score: 760,
+//     time: '3:05',
+//   },
+// ];
 
-jest.mock('react', () => ({
-  ...jest.requireActual('react'),
-  useState: jest
-    .fn()
-    .mockReturnValueOnce([[], jest.fn()])
-    .mockReturnValueOnce([
-      [
-        {
-          id: '1',
-          name: 'Ivan',
-          score: 115,
-          time: '1:15',
-        },
-      ],
-      jest.fn(),
-    ])
-    .mockReturnValueOnce([
-      [
-        {
-          id: '1',
-          name: 'Ivan',
-          score: 115,
-          time: '1:15',
-        },
-        {
-          id: '2',
-          name: 'Roman',
-          score: 318,
-          time: '2:45',
-        },
-        {
-          id: '3',
-          name: 'Ksenia',
-          score: 760,
-          time: '3:05',
-        },
-      ],
-      jest.fn(),
-    ]),
-}));
+// jest.mock('lodash/orderBy', () => ({
+//   default: jest.fn(fn => fn),
+// }));
+
+// jest.mock('react', () => ({
+//   ...jest.requireActual('react'),
+//   useState: jest
+//     .fn()
+//     .mockReturnValueOnce([[], jest.fn()])
+//     .mockReturnValueOnce([
+//       [
+//         {
+//           id: '1',
+//           name: 'Ivan',
+//           score: 115,
+//           time: '1:15',
+//         },
+//       ],
+//       jest.fn(),
+//     ])
+//     .mockReturnValueOnce([
+//       [
+//         {
+//           id: '1',
+//           name: 'Ivan',
+//           score: 115,
+//           time: '1:15',
+//         },
+//         {
+//           id: '2',
+//           name: 'Roman',
+//           score: 318,
+//           time: '2:45',
+//         },
+//         {
+//           id: '3',
+//           name: 'Ksenia',
+//           score: 760,
+//           time: '3:05',
+//         },
+//       ],
+//       jest.fn(),
+//     ]),
+// }));
 
 test('should render placeholder', () => {
-  render(<LeaderBoard />);
+  render(
+    <Provider store={store}>
+      <LeaderBoard />
+    </Provider>
+  );
 
   const placeholder = screen.getByText(
     /данных для рейтинга пока недостаточно/i
@@ -77,24 +87,24 @@ test('should render placeholder', () => {
   expect(placeholder).toBeInTheDocument();
 });
 
-test('should render leaderboard', () => {
-  render(<LeaderBoard />);
+// test('should render leaderboard', () => {
+//   render(<LeaderBoard />);
 
-  const heading = screen.getByRole('heading', { name: /рейтинг игроков/i });
+//   const heading = screen.getByRole('heading', { name: /рейтинг игроков/i });
 
-  expect(heading).toBeInTheDocument();
-});
+//   expect(heading).toBeInTheDocument();
+// });
 
-test('should render all players', () => {
-  render(<LeaderBoard />);
+// test('should render all players', () => {
+//   render(<LeaderBoard />);
 
-  for (const player of players) {
-    const name = screen.getByText(player.name);
-    const score = screen.getByText(player.score);
-    const time = screen.getByText(player.time);
+//   for (const player of players) {
+//     const name = screen.getByText(player.name);
+//     const score = screen.getByText(player.score);
+//     const time = screen.getByText(player.time);
 
-    expect(name).toBeInTheDocument();
-    expect(score).toBeInTheDocument();
-    expect(time).toBeInTheDocument();
-  }
-});
+//     expect(name).toBeInTheDocument();
+//     expect(score).toBeInTheDocument();
+//     expect(time).toBeInTheDocument();
+//   }
+// });
--- packages/client/src/styles/variables/styleConstants.ts
+++ packages/client/src/styles/variables/styleConstants.ts
@@ -8,14 +8,14 @@ export const BORDER_RADIUS_SIZE = '20px';
 
 export const BORDER_WIDTH_TABLE = '2px';
 
-export const COLUMNS_WIDTH_TABLE = '50px 1fr 200px 200px';
+export const COLUMNS_WIDTH_HEAD_TABLE = '50px 1.5fr 1fr 1fr';
 
 export const GRID_TABLE_CONTAINER = css`
   display: grid;
-  grid-template-columns: ${COLUMNS_WIDTH_TABLE};
   gap: ${BORDER_WIDTH_TABLE};
 `;
 
 export const BORDER_BUTTON = `2px solid ${BORDER_COLOR_BUTTON}`;
 
-export const BOX_SHADOW_BUTTON = '0 4px 4px rgba(0, 0, 0, 0.25), inset 4px 4px 4px rgba(130, 128, 128, 0.25)'
+export const BOX_SHADOW_BUTTON =
+  '0 4px 4px rgba(0, 0, 0, 0.25), inset 4px 4px 4px rgba(130, 128, 128, 0.25)';
