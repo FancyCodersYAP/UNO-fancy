08a3c90deb4af48d63eb2a6fd99c6beb8a75c96d
Dormeh <dormeh@icloud.com>
2023-05-18 16:02:12 +0300

    fixes path for sw when build

--- packages/client/src/utils/startSW.ts
+++ packages/client/src/utils/startSW.ts
@@ -2,13 +2,17 @@ export const startServiceWorker = () => {
   // Проверка поддержки браузером service worker'а.
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register("/sw.js")
-      .then(registration => {
-        console.log('ServiceWorker registration successful with  scope: ', registration.scope);
-      })
-      .catch(error => {
-        console.log('ServiceWorker registration failed: ', error);
-      })
-    })
+      navigator.serviceWorker
+        .register('assets/sw.js')
+        .then(registration => {
+          console.log(
+            'ServiceWorker registration successful with  scope: ',
+            registration.scope
+          );
+        })
+        .catch(error => {
+          console.log('ServiceWorker registration failed: ', error);
+        });
+    });
   }
-}
+};
