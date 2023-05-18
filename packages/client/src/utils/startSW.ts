74e276e4c5157ea5b7a8dd958e083a98c6edc56f
catalinadevolg <kattti94@mail.ru>
2023-05-18 18:15:58 +0400

    fix sw for building

--- packages/client/src/utils/startSW.ts
+++ packages/client/src/utils/startSW.ts
@@ -2,13 +2,19 @@ export const startServiceWorker = () => {
   // Проверка поддержки браузером service worker'а.
   if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-      navigator.serviceWorker.register("/sw.js")
-      .then(registration => {
-        console.log('ServiceWorker registration successful with  scope: ', registration.scope);
-      })
-      .catch(error => {
-        console.log('ServiceWorker registration failed: ', error);
-      })
-    })
+      navigator.serviceWorker
+        .register('assets/sw.js', {
+          scope: '/assets/',
+        })
+        .then(registration => {
+          console.log(
+            'ServiceWorker registration successful with  scope: ',
+            registration.scope
+          );
+        })
+        .catch(error => {
+          console.log('ServiceWorker registration failed: ', error);
+        });
+    });
   }
-}
+};
